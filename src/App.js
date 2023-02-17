49ebd8886d3476f0a6679b056e311ad9158f7bf3
Shazammon <ammonorgill@gmail.com>
2023-02-17 16:50:08 -0500

    updated state to add in theme

--- src/App.js
+++ src/App.js
@@ -12,20 +12,24 @@ function App() {
   // })
 
   // const [ count, setCount ] = useState(countInitial())
-  const [ state, setState ] = useState({count: 4, theme: 'blue'})
-  const count = state.count
-  const theme = state.theme
+  const [ count, setCount ] = useState(4)
+  const [ theme, setTheme ] = useState('blue')
+  // const [ state, setState ] = useState({count: 4, theme: 'blue'})
+  // const count = state.count
+  // const theme = state.theme
 
 
   function decrementCount() {
-    // setCount(prevCount => prevCount - 1)
-    setState(prevState => {
-      return { ...prevState, count: prevState.count - 1 }
-    })
+    setCount(prevCount => prevCount - 1)
+    setTheme('red')
+    // setState(prevState => {
+    //   return { ...prevState, count: prevState.count - 1 }
+    // })
   }
 
   function incrementCount() {
-    // setCount(prevCount => prevCount + 1)
+    setCount(prevCount => prevCount + 1)
+    setTheme('blue')
   }
 
   return (
