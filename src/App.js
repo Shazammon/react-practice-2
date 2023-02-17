a21c154281b9191287d1f0d276e4c071154be72b
Shazammon <ammonorgill@gmail.com>
2023-02-16 20:02:15 -0500

    wrote function for decrement count

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React, { useState } from "react";
 function App() {
   const [ count, setCount ] = useState(4)
 
+  function decrementCount() {
+    setCount(count - 1)
+  }
+
   return (
     <>
-    <button>-</button>
+    <button onClick={decrementCount}>-</button>
     <span>0</span>
     <button>+</button>
     </>
