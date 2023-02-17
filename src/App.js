d0f362d88eff69c8b36e0d62e33c9f9cfc0b0074
Shazammon <ammonorgill@gmail.com>
2023-02-17 16:42:43 -0500

    updated functions

--- src/App.js
+++ src/App.js
@@ -18,17 +18,18 @@ function App() {
 
 
   function decrementCount() {
-    setCount(prevCount => prevCount - 1)
+    // setCount(prevCount => prevCount - 1)
   }
 
   function incrementCount() {
-    setCount(prevCount => prevCount + 1)
+    // setCount(prevCount => prevCount + 1)
   }
 
   return (
     <>
     <button onClick={decrementCount}>-</button>
     <span>{count}</span>
+    <span>{theme}</span>
     <button onClick={incrementCount}>+</button>
     </>
   );
