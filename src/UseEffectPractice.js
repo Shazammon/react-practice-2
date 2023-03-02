968d95ed0422dc00e195092740dad09e77d6015d
Shazammon <ammonorgill@gmail.com>
2023-03-02 16:08:57 -0700

    updadted with new console.log to prove out rendering

--- src/UseEffectPractice.js
+++ src/UseEffectPractice.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from "react"
 export default function UseEffectPractice() {
     const [resourceType, setResourceType] = useState('posts')
 
+    console.log('render')
+
     useEffect(() => {
-        console.log('render')
+        console.log('resourcetype changed')
     }, [resourceType]) 
     return (
         <>
