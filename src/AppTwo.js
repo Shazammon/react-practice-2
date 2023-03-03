46e79dc4fa82920c54812a620cf1fa9609aeaee7
Shazammon <ammonorgill@gmail.com>
2023-03-03 16:34:22 -0700

    imported necessary packages

---
+++ src/AppTwo.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import FunctionContextComponent from './FunctionContextComponent'
+import ClassContextComponent from './ClassContextComponent'
+
+export default function AppTwo() {
+
+    return (
+        <>
+            <div>hi</div>
+        </>
+    )
+}
