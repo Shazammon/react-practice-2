1c8a7099c2bf9894b34a87e8a1a30a4242601810
Shazammon <ammonorgill@gmail.com>
2023-03-03 16:57:53 -0700

    finalized darktheme for function component

--- src/FunctionContextComponent.js
+++ src/FunctionContextComponent.js
@@ -2,7 +2,14 @@ import React, { useContext } from "react";
 import { ThemeContext } from "./AppTwo";
 
 export default function FunctionContextComponent() {
+    const darkTheme = useContext(ThemeContext)
+    const themeStyles = {
+        backgroundColor: darkTheme ? '#333' : '#CCC',
+        color: darkTheme ? '#CCC' : '#333',
+        padding: '2rem',
+        margin: '3rem'
+    }
     return (
-            <div>Function Theme</div>
+            <div style={themeStyles}>Function Theme</div>
     )
 }
