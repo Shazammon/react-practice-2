e412a91ed05629d3fad5df3c85b354303443086b
Shazammon <ammonorgill@gmail.com>
2023-03-03 16:59:57 -0700

    created new ThemeContext component

---
+++ src/ThemeContext.js
@@ -0,0 +1,9 @@
+
+
+    const [darkTheme, setDarkTheme] = useState(true)
+
+    function toggleTheme() {
+        setDarkTheme(prevDarkTheme => !prevDarkTheme)
+    }
+
+    export function ThemeProvider({ children })
