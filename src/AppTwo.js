1cf33252354e46b812ca1dd72c7282a5a5739e85
Shazammon <ammonorgill@gmail.com>
2023-03-03 16:36:35 -0700

    created Context components

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react'
 import FunctionContextComponent from './FunctionContextComponent'
 import ClassContextComponent from './ClassContextComponent'
 
+export const ThemeContet = React.createContext()
+
 export default function AppTwo() {
+
+    const
 
     return (
         <>
