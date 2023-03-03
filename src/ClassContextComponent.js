d42e1baa52781b0a84d3b5e4cfd0eb54e7a1449a
Shazammon <ammonorgill@gmail.com>
2023-03-03 16:50:52 -0700

    setting up class component

--- src/ClassContextComponent.js
+++ src/ClassContextComponent.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { ThemeContext } from './App'
 
-export default function ClassContextComponent() {
+export default class ClassContextComponent extends Component {
+    themeStyles(dark) {
+        return {
+            backgroundColor: dark ? '#333' : '#CCC',
+        }
+    }
+
     return (
         <>
             <div>hi</div>
